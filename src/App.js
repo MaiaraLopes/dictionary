ef5b0d1d2db75cf738cf20c7220686beaf5c0b80
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2021-04-09 20:07:42 +0100

    Fixed error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-        <img className="image-top" src="https://i2.wp.com/www.revistaapolice.com.br/wp-content/uploads/2015/09/SindsegSP-lan%C3%A7a-Dicion%C3%A1rio-Bil%C3%ADngue-de-Seguros.jpg?resize=1024%2C682&ssl=1" />
+        <img className="image-top" alt="" src="https://i2.wp.com/www.revistaapolice.com.br/wp-content/uploads/2015/09/SindsegSP-lan%C3%A7a-Dicion%C3%A1rio-Bil%C3%ADngue-de-Seguros.jpg?resize=1024%2C682&ssl=1" />
       </header>
       <main>
         <Dictionary />
